7b5fa1f08f1925100f0793db1edb6ce2ad847ac7
Bhavani-R3 <bhavanirameshr3@gmail.com>
2023-11-25 23:35:57 +0530

    all sections completed

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,34 @@
 let menu = document.querySelector(".nav-links");
 let btn = document.querySelector(".btn-responsive");
+let questions = document.querySelectorAll('.accordion-header');
 
 function toggle() {
     menu.classList.toggle("responsive");
 }
+
+// accordion toggle script
+
+questions.forEach(button => {
+    button.addEventListener('click', function() {
+        const aContent = button.nextElementSibling;
+
+        button.classList.toggle('active');
+
+        if(button.classList.contains('active')) {
+            aContent.style.maxHeight = aContent.scrollHeight + 'px';
+        } else {
+            aContent.style.maxHeight = 0;
+        }
+
+        // close rest other accordion-items
+        questions.forEach(oBtn => {
+            if(oBtn !== button) {
+                oBtn.classList.remove('active');
+                oBtn.nextElementSibling.style.maxHeight = 0;
+            }
+        })
+    })
+});
+
+// year
+document.getElementById("yr").innerText = new Date().getFullYear();
